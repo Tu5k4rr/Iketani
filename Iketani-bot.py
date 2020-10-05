e679b88039c1465b4742490c3753562d8f3c2eee
Tu5k4rr <21346295+Tu5k4rr@users.noreply.github.com>
2020-10-05 12:52:35 +0100

    Create Iketani-bot.py

---
+++ Iketani-bot.py
@@ -0,0 +1,43 @@
+import json
+import discord
+from discord.ext import commands
+import discord.utils
+import re
+
+
+#declaring client
+client = discord.Client()
+
+#Read auth Token file
+with open('foken.txt', 'r') as file:
+    token = file.read()
+
+
+def load_counters():
+    with open('counters.json', 'r') as f:
+       counters = json.load(f)
+    return counters
+
+def save_counters(counters):
+    with open('counters.json', 'w') as f:
+       json.dump(counters, f)
+
+
+@client.event
+async def on_message(message, case_insensitive=True):
+    if message.content.lower() =='oof':
+        counters = load_counters()
+        counters["oof"] += 1
+        channel = message.channel
+        await channel.send(f'Another oof - Total: {str(counters["oof"])}')
+        save_counters(counters)
+    elif message.content.lower() == 'f':
+        counters = load_counters()
+        counters["f"] += 1
+        channel = message.channel
+        await channel.send(f'フ count - Total: {str(counters["f"])}')        
+        save_counters(counters)
+
+
+
+client.run(token)
